1fe35d1e506b51503cda24f1c8d319e0f679f795
Łukasz Sasin <LukSas@Pracownia.com>
2021-09-07 13:27:41 +0200

    first commit

---
+++ zad2.php
@@ -0,0 +1,16 @@
+<?php
+
+$n = $_GET['war'];
+
+for($i=0; $i<100; $i++)
+{
+    
+    $los = rand(58, 65);
+
+echo ($los . "<br>");
+
+?>
+
+<form action="" sethod= "GET">
+    <input type="text" name="war">
+    <input type='submit'  value='Show'>
